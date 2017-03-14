b566e7dc0480f9b4129a17a121af74b77d282321
vinhnguyen2210 <iicanfly2210@gmail.com>
2017-03-14 15:03:25 +0700

    Add static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root "static_pages#home"
+  get "/help", to: "static_pages#help"
+  get "/contact", to: "static_pages#contact"
+  get "/about", to: "static_pages#about"
   resources :users
 end
