f8a239f57b2ad3c1f6fbb73fbfc0a3c1c7136217
vinhnguyen2210 <iicanfly2210@gmail.com>
2017-03-15 18:22:05 +0700

    add static pages use reactjs

---
+++ app/assets/javascripts/components/static_pages/about.es6.jsx
@@ -0,0 +1,10 @@
+class About extends React.Component{
+  render() {
+    return (
+      <div className="center jumbotron">
+        <h1>About</h1>
+        <h2>This is About page</h2>
+      </div>
+    )
+  }
+}
